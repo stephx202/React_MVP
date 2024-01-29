922d63d027fa343da30746cc3a8ff4506dbee65b
stephx202 <stephhernandez202@gmail.com>
2024-01-29 12:32:30 -0800

    line crossing off todo added

--- client/components/Items.jsx
+++ client/components/Items.jsx
@@ -1,25 +1,43 @@
-import React, {useState, useEffect} from 'react';
-
-
+import React, { useState, useEffect } from 'react';
 
 const Items = () => {
-    const [item, setItem] = useState([]);
-  
-    useEffect(() => {
-      fetch("/api/ToDoList")
-        .then((res) => res.json())
-        .then((item) => {
-          setItem(item);
-        });
-    }, []);
-  
-    return (
-      <main>
-        {item.map((item) => (
-          <p className="item" key={item.id} onClick={()=>alert(`click${item.id}`)}> {item.item}</p>
-        ))}
-      </main>
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    fetch("/api/ToDoList")
+      .then((res) => res.json())
+      .then((data) => {
+        // Add a "crossedOff" property to each item
+        const itemsWithCrossedOff = data.map((item) => ({
+          ...item,
+          crossedOff: false,
+        }));
+        setItems(itemsWithCrossedOff);
+      });
+  }, []);
+
+  const handleItemClick = (itemId) => {
+    // Toggle the crossedOff when an item is clicked
+    setItems((prevItems) =>
+      prevItems.map((item) =>
+        item.id === itemId ? { ...item, crossedOff: !item.crossedOff } : item
+      )
     );
   };
 
-export default Items
+  return (
+    <main>
+      {items.map((item) => (
+        <p
+          className={`item ${item.crossedOff ? 'crossed-off' : ''}`}
+          key={item.id}
+          onClick={() => handleItemClick(item.id)}
+        >
+          {item.item}
+        </p>
+      ))}
+    </main>
+  );
+};
+
+export default Items;
