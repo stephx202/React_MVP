d8f7ab34b7474e74492790f517563aafb7d022df
stephx202 <stephhernandez202@gmail.com>
2024-01-30 12:25:21 -0800

    Delete function updated

--- client/components/Items.jsx
+++ client/components/Items.jsx
@@ -14,11 +14,12 @@ const Items = () => {
           ...item,
           crossedOff: false,
         }));
+        //update the state with the modified items
         setItems(itemsWithCrossedOff);
       });
-  }, []);
+  }, []); //add an empty array so that this effect runs only once.
 
-  const completedItem = (itemId) => {
+  const completedItem = (itemId) => { 
     // Toggle the crossedOff property when an item is clicked
     setItems((prevItems) =>
       prevItems.map((item) =>
@@ -34,10 +35,11 @@ const Items = () => {
     })
     .then((res)=>{
       if (res.ok) {
-        setItems((prevItems) => prevItems((item) => item.id !== itemId));
-        console.log(`Successfully deleted item with ID ${itemId}`);
+        //update the state by using .filter to take out deleted items.
+        setItems((prevItems) => prevItems.filter((item) => item.id !== itemId));
+        console.log(`Deleted item ${itemId}`);
       } else {
-        console.error(`Failed to delete item with ID ${itemId}`);
+        console.error(`Unable to delete item  ${itemId}`);
       }
     })
   }
